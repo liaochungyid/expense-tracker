bc5f729814e5bb0c66c09748a102b7d2b9f7b3a7
Liao CY <liaochungyid@gmail.com>
2021-10-25 20:06:52 +0800

    feat: modify index page.

--- app.js
+++ app.js
@@ -6,11 +6,20 @@ if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 const app = express()
 const port = process.env.PORT
 
+const CATEGORY = {
+  家居物業: "https://fontawesome.com/icons/home?style=solid",
+  交通出行: "https://fontawesome.com/icons/shuttle-van?style=solid",
+  休閒娛樂: "https://fontawesome.com/icons/grin-beam?style=solid",
+  餐飲食品: "https://fontawesome.com/icons/utensils?style=solid",
+  其他: "https://fontawesome.com/icons/pen?style=solid"
+}
+
+
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', { CATEGORY: Object.keys(CATEGORY) })
 })
 
 app.get('/users/create', (req, res) => {
